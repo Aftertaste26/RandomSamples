c0b8f38835b520233c39b50cc55e84843d6be6ba
pzexkkb <20hardrock12@gmail.com>
2019-10-06 04:06:35 +0800

    UPDATE

--- EXPORT.js
+++ EXPORT.js
@@ -1,5 +1,32 @@
-let x =['kye','em']
-let y =['osunro','mallare']
-let all =[]
-all = all.concat(x,y)
-console.log(all)
+let name = []
+let array2d = []
+
+//new input
+name.push('chem')
+array2d.push([])
+
+name.push('em')
+array2d.push([])
+
+name.push('kyle')
+array2d.push([])
+
+console.log(name)
+console.log(array2d)
+
+
+//already exist
+for (let count = 0; count < 10; count++) {
+
+    let read = require('readline-sync')
+    let input = read.question('\nName: ')
+    let add = read.question("what to save? : ")
+    let i = name.indexOf(input)
+
+    console.log(i)
+    array2d[i].push(add)
+
+    console.log(name)
+    console.log(array2d)
+
+}
